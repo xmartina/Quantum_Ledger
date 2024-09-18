9546316a70e0ca3a8b89969c0f52cfbeb686211a
xmartina <martinsprince126@gmail.com>
2024-09-18 13:46:49 +0100

    d

--- pages/about/about_content.php
+++ pages/about/about_content.php
@@ -30,7 +30,7 @@
     <div class="demo">
         <div class="container">
             <div class="row">
-                <div class="col-md-4 col-sm-6">
+                <div class="col-md-4 col-sm-6 gy-5">
                     <div class="pricingTable">
                         <h3 class="title">PLATINUM</h3>
                         <div class="price-value">
