13e57ab18922b46d1eb07d40cd2218ecbff769b3
xmartina <martinsprince126@gmail.com>
2024-09-18 13:35:47 +0100

    d

--- pages/about/index.php
+++ pages/about/index.php
@@ -2,5 +2,5 @@
 include_once (__DIR__ . '/../../include/main_function.php');
 $page_name = 'About';
 include_once (__DIR__ . '/../../partials/header.php');
-include_once (__DIR__ . '/../../pages/about/index.php');
+include_once (__DIR__ . '/../../pages/about/main.php');
 include_once (__DIR__ . '/../../partials/footer.php');
---
+++ pages/about/main.php
@@ -0,0 +1,124 @@
+<!-- page header -->
+<header class="header">
+    <div class="overlay"></div>
+    <div class="header-content">
+        <img src="<?=$site_url?>assets/imgs/qfs.png" alt="<?=$site_name?>" width="auto" height="150" style="display: block; margin: 0 auto;">
+        <h1 class="header-title"><?=$site_name?></h1>
+    </div>
+</header><!-- end of page header -->
+
+
+
+<!-- page container -->
+<div class="container page-container">
+    <div class="col-md-10 col-lg-8 m-auto">
+        <h6 class="title mb-4">ABOUT <?=$site_name?></h6>
+        <p class="mb-5">QFS literal meaning is Quantum Financial System, which is an advanced financial system launched to eradicate monopoly on monetary system.The global financial reset has started and the quantum financial system that will replace all banks after the world financial crash is live.Quantum Financial System would be a breakthrough in the world of banking which will lead to a new era of banking. QFS will not be influenced by Government policies, rather it will be entirely backed by tangible assets like Gold, Platinum, Oil and will not be based upon mere piece of papers which have no evidentiary value.<br><br></p><div class="image-container">
+            <img src="qfsho2.jpg" alt="" class="image">
+        </div><br><br>
+
+        <h3>KEY FEATURES OF QUANTUM FINANCIAL SYSTEM </h3>
+
+        <br><p>
+            1)It encapsulates a global network for the transmission of asset-backed digital currency.<br><br>2) It will replace the centrally controlled SWIFT System in the US.<br><br>3)It protects all involved parties from corruption and manipulation within the banking system.<br><br>4)It operates independently of the current decentralized system.<br><br>5)It is not a cryptocurrency, but a digital currency backed by assets.</p>
+        <br><br><div class="image-container">
+            <img src="qfsho.jpg" alt="" class="image">
+        </div><br><br><p>
+
+            The History of money is entering a new era, You might just wake up with no money.Convert all your fiat currency into a digital backed currency.We walk you through that journey.Our System is designed to convert your assets into a digitally backed funds.<br>
+            The Quantum Financial System (QFS) Holdings is a universal network that is developed to facilitate the transfer of asset-backed funds,QFS HOLDINGS is a cutting-edge financial technology that brings a fresh perspective to the monetary world.This technology is deeply rooted in quantum-secure blockchain encryption, quantum computing, and quantum cryptography backed by gold and silver.</p><br><br><br><p>
+            QFS holdings also provide financial advise  on the purchase,holding,and liquidation of tangible assets(i.e gold and silver etc)for both retirement and cash accounts and also offers brokerage, securities, cryptocurrencies, asset allocation, and trading services with incorporating quantum security solutions.<br><br>This financial industry is no stranger to the task of portfolio optimization and Quantum Investment which involves selecting the best possible investment portfolio out of the set of all portfolios being considered based on expected return and risk free as revolutionize areas such as risk management, asset pricing, and algorithmic trading. <br><br>
+
+            One of the main advantages of Quantum Financial System holdings is that it provides an extra layer of security with QFS Card which you can use anywhere in the world for making payment.All you have to do is swipe your card and the payment amount is deducted from the available balance of your digital ISO 20022 assets which adheres to the QFS standard for electronic interchange .Imagine a world where financial transactions are completed almost instantaneously.<br><br>
+
+
+        </p>
+    </div><br><br>
+    <h3>INVESTMENT PLANS</h3><br>
+
+    <div class="demo">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-sm-6">
+                    <div class="pricingTable">
+                        <h3 class="title">PLATINUM</h3>
+                        <div class="price-value">
+                            <span class="currency">$</span>
+                            <span class="amount">100</span>
+                            <span class="month">Start From</span>
+                        </div>
+                        <ul class="pricing-content">
+                            <li><b>Min Deposit</b> $100</li>
+                            <li><b>Max Deposit</b> $9,999</li>
+                            <li><b>ROI</b> 2% Daily</li>
+                            <li><b>Duration</b> 3days</li>
+
+                        </ul>
+                        <a href="user/login.html" class="pricingTable-signup">Invest Now</a>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                    <div class="pricingTable pink">
+                        <h3 class="title">DIAMOND</h3>
+                        <div class="price-value">
+                            <span class="currency">$</span>
+                            <span class="amount">10,000</span>
+                            <span class="month">Start From</span>
+                        </div>
+                        <ul class="pricing-content">
+                            <li><b>Min Deposit</b> $10,000</li>
+                            <li><b>Max Deposit</b> $29,999</li>
+                            <li><b>ROI</b> 5% Daily</li>
+                            <li><b>Duration</b> 7days</li>
+
+                        </ul>
+                        <a href="user/login.html" class="pricingTable-signup">Invest Now</a>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                    <div class="pricingTable pink">
+                        <h3 class="title">SILVER</h3>
+                        <div class="price-value">
+                            <span class="currency">$</span>
+                            <span class="amount">30,000</span>
+                            <span class="month">Start From</span>
+                        </div>
+                        <ul class="pricing-content">
+                            <li><b>Min Deposit</b> $30,000</li>
+                            <li><b>Max Deposit</b> $49,999</li>
+                            <li><b>ROI</b> 15% Daily</li>
+                            <li><b>Duration</b> 14days</li>
+
+                        </ul>
+                        <a href="user/login.html" class="pricingTable-signup">Invest Now</a>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                    <div class="pricingTable blue">
+                        <h3 class="title">GOLD</h3>
+                        <div class="price-value">
+                            <span class="currency">$</span>
+                            <span class="amount">50,000</span>
+                            <span class="month">Start From</span>
+                        </div>
+                        <ul class="pricing-content">
+                            <li><b>Min Deposit</b> $50,000</li>
+                            <li><b>Max Deposit</b> $1,000,000</li>
+                            <li><b>ROI</b> 25% Daily</li>
+                            <li><b>Duration</b> 28days</li>
+
+                        </ul>
+                        <a href="user/login.html" class="pricingTable-signup">Invest Now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><br><br>
+
+    <h4>JOIN THE BIGGEST FINANCIAL REVOLUTIONARY SYSTEM DESIGNED TO PROTECT YOUR FINANCES FROM WORLD FINANCIAL CRASH.</h4>
+
+
+
+
+    <!-- row -->
+        
