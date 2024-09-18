a240e9e1d8e9cc33e403a7e60471ec0bee51c5ff
xmartina <martinsprince126@gmail.com>
2024-09-18 13:28:17 +0100

    d

--- pages/about/index.php
+++ pages/about/index.php
@@ -1,1 +1,6 @@
 <?php
+include_once (__DIR__ . '/../../include/main_function.php');
+$page_name = 'About';
+include_once (__DIR__ . '/../../partials/header.php');
+include_once (__DIR__ . '/../../pages/about/index.php');
+include_once (__DIR__ . '/../../partials/footer.php');
